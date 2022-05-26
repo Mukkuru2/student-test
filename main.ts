947ab61275a775c6a4b925ad8eb20b7ef48c9b07
Dorus van Weelden <48964839+Mukkuru2@users.noreply.github.com>
2022-05-26 14:48:39 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,6 @@
+basic.forever(function () {
+    music.playTone(262, music.beat(BeatFraction.Half))
+    music.playTone(311, music.beat(BeatFraction.Half))
+    music.playTone(294, music.beat(BeatFraction.Quarter))
+    music.playTone(233, music.beat(BeatFraction.Quarter))
+})
